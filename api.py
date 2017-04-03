fa37f6631bcca79cdef46838aeb1826967494d82
Zach Hauser <zehauser@gmail.com>
2017-04-02 22:10:03 -0700

    initial

--- api.py
+++ api.py
@@ -1,9 +1,26 @@
 import webapp2
+import os
+import MySQLdb
+
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
 
 class ApiServer(webapp2.RequestHandler):
-    def get(self, args):
+    def get(self, arg):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write("API Server: {}".format(args))
+        cloudsql_unix_socket = os.path.join(
+          '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+        db = MySQLdb.connect(
+          unix_socket=cloudsql_unix_socket,
+          user=CLOUDSQL_USER)
+        cursor = db.cursor()
+        cursor.execute('USE testing')
+        cursor.execute('SELECT lname, fav_color FROM Users WHERE fname = %s', (arg,))
+        if cursor.rowcount:
+          for (lname, color) in cursor.fetchall():
+            self.response.write('{} {}\'s favorite color is {}\n'.format(arg, lname, color))
+        else:
+          self.response.write("No user with first name: {}\n".format(arg))
 
 app = webapp2.WSGIApplication([
     ('/api/(.*)', ApiServer),
